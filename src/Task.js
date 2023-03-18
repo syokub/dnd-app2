a46d4c41086b73b7a94e64a3e2a73a02b5fd8c6d
Yoqubjon Sobirjonov <105698597+syokub@users.noreply.github.com>
2023-03-18 19:45:00 +0500

    created

--- src/Task.js
+++ src/Task.js
@@ -1,21 +1,13 @@
-import React from "react";
+import React, { memo } from "react";
 import { Draggable } from "react-beautiful-dnd";
 import CardComponent from "./Card";
+
+const MemoizedCard = memo(CardComponent);
 
 function Task({ task, index }) {
 	return (
 		<Draggable draggableId={task.id} index={index}>
-			{(provided) => (
-				// <div
-				// 	className="task"
-				// 	{...provided.draggableProps}
-				// 	{...provided.dragHandleProps}
-				// 	ref={provided.innerRef}
-				// >
-				// 	{task.content}
-				// </div>
-				<CardComponent el={task} provided={provided} />
-			)}
+			{(provided) => <MemoizedCard el={task} provided={provided} />}
 		</Draggable>
 	);
 }
